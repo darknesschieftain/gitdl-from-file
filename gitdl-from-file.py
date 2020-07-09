69dfaa650add0187fbaa698ef01e127adf3079c9
darkness chieftain <55187733+darknesschieftain@users.noreply.github.com>
2020-07-09 22:05:37 +0430

    Add files via upload

---
+++ gitdl-from-file.py
@@ -0,0 +1,15 @@
+import os
+print(" ----------------------------- ")
+print("| github downloader from file  |")
+print("| created by darkness chieftain|"
+print(" ----------------------------- ")
+
+print("example of path for repos file = /home/user/dlfiles.txt")
+filepath = input("please enter github repos file path = ")
+file = open(filepath,"r")
+lines = file.readlines()
+linescount = len(lines)
+i = 0
+while(i<=linescount):
+    os.popen("git clone " + lines[i] )
+    i = i+1
